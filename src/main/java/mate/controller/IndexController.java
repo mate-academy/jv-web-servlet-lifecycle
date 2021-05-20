280507c776894dc0343c9e99e7fa3b08d649bbfb
VladislavTetar <luxeon010203@gmail.com>
2021-05-20 17:19:43 +0300

    Fixed doGet() method

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -22,7 +22,6 @@ public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        super.doGet(req, resp);
         req.getRequestDispatcher("WEB-INF/view/index.jsp").forward(req, resp);
         myResource.write(LocalDateTime.now().toString());
     }
