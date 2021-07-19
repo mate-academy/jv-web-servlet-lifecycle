6be6ea7b60fa541acea40529aeaf66f04e84f762
Vova Bilodon <vovabilodon@gmail.com>
2021-07-19 16:55:43 +0300

    fixed style mistakes

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,15 +1,13 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalTime;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
-
-import java.io.IOException;
-import java.time.LocalTime;
 
 @WebServlet(urlPatterns = "/info")
 public class IndexController extends HttpServlet {
@@ -22,7 +20,8 @@ public class IndexController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         String date = LocalTime.now().toString();
         myResource.write(date);
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
