8a1702ce19ba4d33c5c187870c13b21ee7cb29c0
vKutik <84785564+vKutik@users.noreply.github.com>
2021-07-19 16:04:05 +0300

    add servlet

--- src/main/java/mate/controller/InfoController.java
+++ src/main/java/mate/controller/InfoController.java
@@ -1,33 +1,17 @@
 package mate.controller;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mate.dao.MyCoolResource;
 
-@WebServlet(urlPatterns = "/index")
-public class IndexController extends HttpServlet {
-    private MyCoolResource myResource;
-
-    @Override
-    public void init()
-    throws ServletException {
-        myResource = MyCoolResource.openResource();
-    }
-
+@WebServlet (urlPatterns = "/info")
+public class InfoController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, IOException {
-        myResource.write(LocalDateTime.now().toString());
         req.getRequestDispatcher("/META-INF/views/info.jsp").forward(req, resp);
-    }
-
-    @Override
-    public void destroy() {
-        myResource.close();
     }
 }
