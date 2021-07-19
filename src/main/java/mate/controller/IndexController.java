79c2636fb4ece2a74215fce7a6fdc047d12a41b3
Benito <zara.benito17@gmail.com>
2021-07-19 16:11:42 +0300

    fixed checkstyle;

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -3,11 +3,13 @@ package mate.controller;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
 
