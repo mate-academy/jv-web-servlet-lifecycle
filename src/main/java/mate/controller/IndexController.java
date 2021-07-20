796e1006d750fe8901c2de7a6b79acd2087dd607
vKutik <84785564+vKutik@users.noreply.github.com>
2021-07-20 12:22:08 +0300

    rename file remove coment move throws block

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -14,8 +14,7 @@ public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
 
     @Override
-    public void init()
-            throws ServletException {
+    public void init() throws ServletException {
         myResource = MyCoolResource.openResource();
     }
 
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Doxit
-  Date: 19.07.2021
-  Time: 15:56
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
