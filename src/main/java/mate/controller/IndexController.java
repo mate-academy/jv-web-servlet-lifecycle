fa0a570da4421cd1fd1455f20da18ad1a44301e4
Daniel Kuruch <kuruch1004@gmail.com>
2021-07-20 12:43:41 +0300

    refactored code

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -19,7 +19,7 @@ public class IndexController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         myResource.write(LocalDateTime.now().toString());
         req.getRequestDispatcher("/WEB-INF/views/siteTemplate.jsp").forward(req,resp);
