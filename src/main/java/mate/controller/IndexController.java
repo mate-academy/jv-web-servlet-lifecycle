abebd511c1d3455e6fe496fa221445e6f05188cb
Dana Khromenko <danadiadius@gmail.com>
2021-08-13 18:13:17 +0300

    Implemented IndexController

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,34 @@
 package mate.controller;
 
-import javax.servlet.http.HttpServlet;
+import java.time.LocalDateTime;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
+import java.io.IOException;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String currentDateText = "Current date: " + LocalDateTime.now();
+        myResource.write(currentDateText);
+        req.setAttribute("current_date", currentDateText);
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
 }
