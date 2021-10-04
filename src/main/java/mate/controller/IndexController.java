221de146cd54a225a9c16b81f0e67a3e6a19c3a6
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-10-04 22:36:37 +0300

    Task lifecycle servlet was solved

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,41 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    static {
+        System.out.println("static IndexController");
+    }
+
+    public IndexController() {
+        System.out.println("constructor IndexController");
+    }
+
+    @Override
+    public void init() {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        System.out.println("method doGet IndexController");
+        myResource.write(LocalDateTime.now().toString());
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
