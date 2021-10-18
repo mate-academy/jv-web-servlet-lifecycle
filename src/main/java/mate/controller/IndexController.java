0637eaceae6875a7e696100c9f083f8012f4684b
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-10-18 23:50:32 +0300

    mvn fix

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,14 +1,12 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
-
-import java.io.IOException;
-import java.time.LocalDateTime;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
@@ -19,7 +17,8 @@ public class IndexController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         myResource.write(LocalDateTime.now().toString());
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
