d4efe6c61cb5de6403482755877631e3b0fdc759
Oleksii-kho <oleksiikhoriev@gmail.com>
2021-12-09 17:12:46 +0200

    changed urlPatterns

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
-@WebServlet(urlPatterns = "")
+@WebServlet(urlPatterns = "/")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
 
