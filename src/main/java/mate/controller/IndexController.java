99ae1d0a71a7aed56fa2ba19544b72a83d10e79a
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-04 22:18:09 +0200

    Remove WebServlet from IndexController

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
-@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private static final String DATETIME_PATTERN = "HH:mm:ss MMM dd yyyy";
     private static final String RESOURCE_WRITTEN = "WEB-INF/views/resourceWrittenOk.jsp";
