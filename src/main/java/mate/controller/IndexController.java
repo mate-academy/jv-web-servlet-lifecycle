baafbf086618aa0ab801b64120c72295bdeac4df
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-04 21:41:16 +0200

    Fix checkstyle errors in IndexController

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,20 +1,20 @@
 package mate.controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import mate.dao.MyCoolResource;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
-    private MyCoolResource myResource;
     private static final String DATETIME_PATTERN = "HH:mm:ss MMM dd yyyy";
-    private static final String RESOURCE_WRITTEN = "src/main/webapp/WEB-INF/views/resourceWrittenOk.jsp";
+    private static final String RESOURCE_WRITTEN = "WEB-INF/views/resourceWrittenOk.jsp";
+    private MyCoolResource myResource;
 
     @Override
     public void init() {
@@ -22,8 +22,11 @@ public class IndexController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        myResource.write(LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATETIME_PATTERN)));
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        myResource.write(
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATETIME_PATTERN))
+        );
         req.getRequestDispatcher(RESOURCE_WRITTEN).forward(req, resp);
     }
 
