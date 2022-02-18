eb4d3bade70423afb8a2007eb289ce670c41d7c3
djmolot <djmolot@gmail.com>
2022-02-18 11:43:46 +0200

    deleting comments & sout`s

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -15,20 +15,10 @@ import mate.dao.MyCoolResource;
 @WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
 
-    static {
-        System.out.println(
-                "Servlet container Tomcat was loaded class IndexController to class loader");
-    }
-
     private MyCoolResource myResource;
-
-    public IndexController() {
-        System.out.println("Instance of IndexController class was created");
-    }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        System.out.println("Method init was called. Here we can initialize field myResource");
         super.init(config);
         myResource = MyCoolResource.openResource();
     }
@@ -36,7 +26,6 @@ public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        System.out.println("Method doGet was called");
         LocalDateTime now = LocalDateTime.now();
         String formatDateTime = formatDateTime(now);
         myResource.write(formatDateTime);
@@ -45,16 +34,12 @@ public class IndexController extends HttpServlet {
 
     @Override
     public void destroy() {
-        System.out.println("Method destroy() was called");
         super.destroy();
     }
 
-    public String formatDateTime(LocalDateTime dateTime) {
+    private String formatDateTime(LocalDateTime dateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm:ss")
                 .withLocale(Locale.ENGLISH);
-        //MMMM: Exactly 4 pattern letters will use the full form
-        //(The count of pattern letters determines the format)
-        //HH: 24 hour pattern, hh: 12 hour pattern
         return dateTime.format(formatter);
     }
 }
