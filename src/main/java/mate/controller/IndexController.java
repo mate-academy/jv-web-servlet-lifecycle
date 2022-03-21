4e5f932b17cd69734b33ed00f67e09aa3c352f75
sltnsrh <lifenjoy8888@gmail.com>
2022-03-21 16:27:39 +0200

    IndexController implemented and index.jsp created

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,43 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    static {
+        System.out.println("IndexController was loaded");
+    }
+
+    public IndexController() {
+        System.out.println("IndexController was called");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String stringLocalDateTime = localDateTime.toString();
+        myResource.write(stringLocalDateTime);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
