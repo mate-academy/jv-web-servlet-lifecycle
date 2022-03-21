19857b170264a538b5b664ef60ad81ee1207f506
vladsysa <vladsysa@mail.ru>
2022-03-21 10:28:50 +0100

    First commit

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,45 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    static {
+        System.out.println("IndexController was loaded...");
+    }
+
+    public IndexController() {
+        System.out.println("IndexController was called...");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String formatPattern = localDateTime
+                .format(DateTimeFormatter.ofPattern("dd-yyyy-MM HH:mm"));
+        myResource.write(formatPattern);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
