1ba67abee8d6ce10f5a1f427e1d7dba1dfacf04a
Angela1608 <89303267+Angela1608@users.noreply.github.com>
2022-04-28 19:33:03 +0300

    solved jv-web-servlet-lifestyle

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,31 @@
 package mate.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        LocalDate localDate = LocalDate.now();
+        myResource.write(String.valueOf(localDate));
+        req.getRequestDispatcher("/webapp/info.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
