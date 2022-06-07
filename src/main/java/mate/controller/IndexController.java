82e4787fec05727fedd672b8dbbf2fc93efed558
Lyudmila D <dunda94lyudmila@gmail.com>
2022-06-07 17:41:22 +0300

    servlet created

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,29 @@
 package mate.controller;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+        myResource.write("Date: 07.06.2022. Time: 17:32");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
