c6b3f916774c1164cca4c3c70a7cfc43426b7378
Maksym Karamavrov <makcumkaka@gmail.com>
2022-07-09 17:16:19 +0300

    First commit. Application is working.

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,34 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String dateAndTime = LocalDate.now() + " " + LocalTime.now();
+        myResource.write("Current date and time is: " + dateAndTime);
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        myResource.close();
+    }
 }
