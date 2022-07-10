077e0777acfde6525cb9ad92f05764082049c671
agash <alexgash8@gmail.com>
2022-07-10 22:21:09 +0300

    cant get why redirect doesn't work and why sout doesnt work

--- src/main/java/mate/controller/ItemsController.java
+++ src/main/java/mate/controller/ItemsController.java
@@ -1,23 +1,22 @@
 package mate.controller;
 
+import mate.dao.MyCoolResource;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import mate.dao.MyCoolResource;
-
 import java.io.IOException;
 
-public class IndexController extends HttpServlet {
-    private final String path = "/WEB-INF/views/index.jsp";
+public class ItemsController extends HttpServlet {
+    private final String path = "/WEB-INF/views/items.jsp";
     private MyCoolResource myResource;
     //TODO: implement
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/items").forward(req, resp);
-//        resp.sendRedirect("/items");
+        System.out.println("items controller was called");
+        req.getRequestDispatcher(path).forward(req, resp);
     }
 }
---
+++ src/main/webapp/WEB-INF/views/items.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html; UTF-8" %>
+<html lang="EN">
+<head>
+    <title>addition</title>
+</head>
+<body>
+<h1>Here is another page</h1>
+</body>
+</html>
