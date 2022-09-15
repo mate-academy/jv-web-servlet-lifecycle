b0105c7d821443884bbf27b9822ad666eb8a3943
X3NON-135 <immort4ld3momo@gmail.com>
2022-09-15 15:26:17 +0100

    fixed checkstyle

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,20 +1,15 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
-
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    private LocalDateTime localDateTime;
 
     @Override
     public void init() throws ServletException {
@@ -23,7 +18,8 @@ public class IndexController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         myResource.write(LocalDateTime.now().toString());
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
         System.out.println(myResource);
