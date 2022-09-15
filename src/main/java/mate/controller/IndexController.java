8da1fe6772c54f46adf893ee46a65faf48e39df7
Anna Popova <anitapopova1996@gmail.com>
2022-09-15 11:52:31 +0300

    Implemented doGet() , created servlet lifecycle

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,38 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        myResource = MyCoolResource.openResource();
+        super.init(config);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .format(Calendar.getInstance().getTime());
+        System.out.println("THE DATE : " + timeStamp);
+        myResource.write(timeStamp);
+        req.setAttribute("date", timeStamp);
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+        super.destroy();
+    }
 }
