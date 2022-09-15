7aa9a92397648d99f4b55ad97ff5e99975056696
LubusUzh <lubus.uzh@gmail.com>
2022-09-16 02:01:11 +0300

    Add: realizations to init and destroy methods

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,29 +1,31 @@
 package mate.controller;
 
+import java.io.IOException;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
-
-import java.io.IOException;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
 
     @Override
-    public void init() throws ServletException {
-        super.init();
+    public void init(ServletConfig config) throws ServletException {
+        myResource = MyCoolResource.openResource();
+        super.init(config);
     }
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         super.doGet(req, resp);
     }
 
     @Override
     public void destroy() {
+        myResource.close();
         super.destroy();
     }
 }
