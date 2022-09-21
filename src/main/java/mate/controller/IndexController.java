4788236643c5398459c40050fdacbdc7869d39b0
Serhii Buria <sergeiburya@gmail.com>
2022-09-21 15:46:12 +0300

    added solution task

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,37 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String currentDateTime = LocalDateTime.now().toString();
+        String viewPage = "/WEB-INF/views/index.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPage);
+        dispatcher.forward(req, resp);
+        myResource.write(currentDateTime);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
