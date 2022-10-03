debb1594b9dcab488df0a02a727e9466ff333ab8
Igor Petrichenko <i.petrichenko@gmail.com>
2022-10-03 13:17:19 +0300

    HW-WEB-SERVLET ver2

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,14 +1,13 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
