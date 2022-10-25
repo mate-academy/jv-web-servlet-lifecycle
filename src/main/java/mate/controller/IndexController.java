158bb0a15cafac4c17174620c1de3f72b55c7a2a
DooMLeoN <levberezov01@gmail.com>
2022-10-25 16:32:54 +0300

    added solution

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,44 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
-    private MyCoolResource myResource;
-    //TODO: implement
+    private MyCoolResource myCoolResource;
+
+    static {
+        System.out.println("Servlet loaded");
+    }
+
+    public IndexController() {
+        System.out.println("Servlet was called");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        myCoolResource = MyCoolResource.openResource();
+        super.init();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        myCoolResource.write(LocalDateTime.now().toString());
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        myCoolResource.close();
+    }
 }
+
