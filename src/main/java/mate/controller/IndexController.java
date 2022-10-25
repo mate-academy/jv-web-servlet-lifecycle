c5f67c2b6a6516a6581566f7f585b891706ebb52
DooMLeoN <levberezov01@gmail.com>
2022-10-25 16:35:45 +0300

    added solution

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -29,8 +29,8 @@ public class IndexController extends HttpServlet {
 
     @Override
     public void destroy() {
-        super.destroy();
         myCoolResource.close();
+        super.destroy();
     }
 }
 
