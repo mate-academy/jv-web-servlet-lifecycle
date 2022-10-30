07aaa1a045c7aad5470ad4d51bae5ec1fccdb8e6
sasha <radovietsoleksandr@gmail.com>
2022-10-30 13:07:47 +0200

    w1

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,38 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        MyCoolResource.openResource();
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        myResource = MyCoolResource.openResource();
+        LocalDate localDate = LocalDate.now();
+        String timeNow = String.valueOf(localDate);
+        req.getRequestDispatcher("/WEB-INF/views/jspIndex.jsp").include(req,
+                resp);
+
+
+        destroy();
+
+    }
 }
