a0b1eb419a4b6004d026c1f2a9df625edfc398c5
Anton Pavliuk <antonpavliuk@Antons-Air.Dlink>
2022-11-10 14:12:03 +0200

    Some style corrections

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,15 +1,12 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
-
-import java.io.IOException;
-import java.time.LocalDateTime;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
@@ -21,9 +18,11 @@ public class IndexController extends HttpServlet {
     }
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         myResource.write(LocalDateTime.now().toString());
-        req.getRequestDispatcher("WEB-INF/views/index.jsp").include(req, resp);
+        req.getRequestDispatcher("WEB-INF/views/index.jsp")
+                .include(req, resp);
         System.out.println("GET");
     }
 
