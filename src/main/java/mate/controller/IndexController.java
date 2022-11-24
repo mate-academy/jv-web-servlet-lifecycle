6418ed2d16c3a2238f73bc14c301c332384217d1
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-11-24 12:29:38 +0200

    created doGet

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,22 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalTime;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
+
+@WebServlet(urlPatterns = "/index")
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        myResource.write(LocalTime.now().toString());
+        req.getRequestDispatcher("WEB-INF/views/info.jsp").forward(req, resp);
+    }
 }
