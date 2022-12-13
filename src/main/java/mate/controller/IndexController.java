66185e84823f9b7cd342947553a3a8b8077f3abe
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-13 14:43:30 +0200

    added logic in IndexController.java

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,38 @@
 package mate.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import mate.dao.MyCoolResource;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
     //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        myResource.write(String.valueOf(LocalDate.now()));
+        req.getRequestDispatcher("/").include(req, resp);
+        myResource.write(String.valueOf(LocalTime.now()));
+        req.getRequestDispatcher("/").include(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+        super.destroy();
+    }
 }
