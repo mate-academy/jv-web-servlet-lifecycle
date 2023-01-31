85816bffaca60e535efc9aff0387feee48fb49e9
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2023-01-31 16:31:53 +0200

    v01

---
+++ src/test/java/mate/controller/IndexControllerTest.java
@@ -0,0 +1,18 @@
+package mate.controller;
+
+import org.junit.Test;
+
+public class IndexControllerTest {
+
+    @Test
+    public void init() {
+    }
+
+    @Test
+    public void doGet() {
+    }
+
+    @Test
+    public void destroy() {
+    }
+}
