db160c3323a7854e49224d257062457f2155cdfc
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-02-01 04:16:50 +0200

    servlet lifecycle task completed

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,37 @@
 package mate.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import mate.dao.MyCoolResource;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        myResource.write("Current date is: " + LocalDateTime.now());
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+        super.destroy();
+    }
 }
