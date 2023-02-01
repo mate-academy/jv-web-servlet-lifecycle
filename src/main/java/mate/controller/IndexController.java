d990746fe943fde127e07218e6839c8ec76cef5e
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-02-01 04:21:45 +0200

    checkstyle fix

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,18 +1,17 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
-
-import java.io.IOException;
-import java.time.LocalDateTime;
 
 @WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
+    private MyCoolResource myResource;
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -20,8 +19,6 @@ public class IndexController extends HttpServlet {
         myResource.write("Current date is: " + LocalDateTime.now());
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
-
-    private MyCoolResource myResource;
 
     @Override
     public void init() throws ServletException {
