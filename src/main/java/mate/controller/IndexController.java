f2b4bef6ea024101e1d5cbc84a4fb17d5a095e77
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-02-01 14:57:05 +0100

    servlet solution

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,24 +1,25 @@
 package mate.controller;
 
+import java.io.IOException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
-
-import java.io.IOException;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         myResource = MyCoolResource.openResource();
+        super.init(config);
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         myResource.write("Just some text");
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
@@ -26,5 +27,6 @@ public class IndexController extends HttpServlet {
     @Override
     public void destroy() {
         myResource.close();
+        super.destroy();
     }
 }
