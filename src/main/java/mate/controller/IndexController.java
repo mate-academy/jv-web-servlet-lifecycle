851042cf477111bfed6b33f05ef739390909df15
Dmytro Koval <dimonkoval@gmail.com>
2023-03-03 01:27:42 +0200

    1

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,40 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        myResource.close();
+    }
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        LocalDateTime date = LocalDateTime.now();
+        ArrayList<String> list = new ArrayList<>();
+        list.add(String.valueOf(date));
+        myResource.write(String.valueOf(date));
+        req.setAttribute("message", String.valueOf(date));
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
 }
