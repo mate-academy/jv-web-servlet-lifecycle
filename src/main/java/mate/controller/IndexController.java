6434dc00e6feff17b28cee3a6978350a9e6e8ab3
Dmytro Katrevych <katrevichdv@gmail.com>
2023-03-07 13:16:37 +0200

    maven-war-plugin version was added to pom.xml IndexController.java checkstyle was fixed

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -6,26 +6,25 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
-	private MyCoolResource myResource;
+    private MyCoolResource myResource;
 
-	@Override
-	public void init() {
-		myResource = MyCoolResource.openResource();
-	}
+    @Override
+    public void init() {
+        myResource = MyCoolResource.openResource();
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
-			throws ServletException, IOException {
-		myResource.write(LocalDateTime.now().toString());
-		req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        myResource.write(LocalDateTime.now().toString());
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
 
-	@Override
-	public void destroy() {
-		myResource.close();
-	}
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
