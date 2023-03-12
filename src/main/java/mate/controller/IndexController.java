4d93dede4c48946c35cd7a7a3f622bc7c3727514
Watxesnavo <112902418+Watxesnavo@users.noreply.github.com>
2023-03-12 23:34:09 +0100

    try

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -5,14 +5,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mate.dao.MyCoolResource;
 import java.io.IOException;
+import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         req.setAttribute("data", myResource);
         myResource.write("1");
         myResource.write("2");
