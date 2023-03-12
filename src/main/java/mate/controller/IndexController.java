df33658e03fca92fd6432c801f708a451e0590fd
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2023-03-12 16:34:36 +0200

    initial commit

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,36 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+            throws ServletException, IOException {
+        myResource.write(formatter.format(new Date(System.currentTimeMillis())));
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+        //next output for test
+        System.out.println(myResource.toString());
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
