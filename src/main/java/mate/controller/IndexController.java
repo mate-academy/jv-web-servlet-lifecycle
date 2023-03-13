79ebd2114783dbb34d4edf4bcd21a01e23f6ded3
Vasyl Piznak <piznak3145@gmail.com>
2023-03-13 23:51:10 +0200

    Finish

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,28 +1,31 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
 
-import java.io.IOException;
-
 public class IndexController extends HttpServlet {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+
     private MyCoolResource myResource;
 
     @Override
-    public void init() throws ServletException {
-
-        System.out.println("Всім ку");
+    public void init() {
+        myResource = MyCoolResource.openResource();
     }
-
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        System.out.println("Добрий вечір");
+        String dateTimeNow = LocalDateTime.now().format(DATE_TIME_FORMATTER);
+        System.out.println(dateTimeNow);
+        myResource.write(dateTimeNow);
         req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req,resp);
     }
 
