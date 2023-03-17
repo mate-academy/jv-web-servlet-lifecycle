61a7a8b96e2be46de67b912d6bc9c44cbf1b1537
Sergiy Golubchenko <galuba1@gmail.com>
2023-03-17 23:02:33 +0200

    Use init(), doGet, destroy  and call MyCoolResource method

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,36 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        myResource = MyCoolResource.openResource();
+        myResource.write("Slava Ukraini!");
+        myResource.write("Heroyam slava!");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        LocalDate localDate = LocalDate.now();
+        myResource.write(localDate.toString());
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>index</title>
+</head>
+<body>
+<h1>Slava Ukraine!</h1>
+</body>
+</html>
