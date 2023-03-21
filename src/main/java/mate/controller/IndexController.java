bbe3a208b9f3a857f3777b0e9706954ee0952690
Serhii Shyian <serhii.shiyan@ukr.net>
2023-03-21 19:39:11 +0200

    Added files: index.jsp, MyCoolMoment.java, modified IndexController class (override init(), doGet(), destroy() methods) and pom.xml

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,39 @@
 package mate.controller;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
+import mate.service.MyCoolMoment;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
+    private final MyCoolMoment myCoolMoment;
     //TODO: implement
+
+    public IndexController() {
+        myCoolMoment = new MyCoolMoment();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String moment = myCoolMoment.getFormatter();
+        myResource.write(moment);
+        req.getSession().setAttribute("moment", moment);
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+        super.destroy();
+    }
 }
