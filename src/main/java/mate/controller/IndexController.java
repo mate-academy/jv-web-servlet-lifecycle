6cbebb0008590d15ac2248c5412c50583ce95f1c
Nickolay Voloshchuk <124300295+NickolayVoloshchuk@users.noreply.github.com>
2023-04-02 22:46:02 +0300

    HomeWork Java WEB/Servlet Lifecycle

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,42 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
+    private static final String DATE = "Date";
+    private static final String TIME = "Time";
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        myResource = MyCoolResource.openResource();
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String currentDate = DATE + ": " + LocalDate.now();
+        String currentTime = TIME + ": " + LocalTime.now();
+        myResource.write(currentDate);
+        myResource.write(currentTime);
+        req.setAttribute(DATE, currentDate);
+        req.setAttribute(TIME, currentTime);
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+        super.destroy();
+    }
 }
