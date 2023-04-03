2a522a643ad4507b090458c86e996345f1e14892
copypasterr <114933128+copypasterr@users.noreply.github.com>
2023-04-03 17:50:19 +0300

    draft

--- src/main/java/mate/controller/ItemsController.java
+++ src/main/java/mate/controller/ItemsController.java
@@ -4,18 +4,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import mate.dao.MyCoolResource;
-
 import java.io.IOException;
 
-public class IndexController extends HttpServlet {
-    private MyCoolResource myResource;
-
+public class ItemsController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("IndexController was called");
-        req.getRequestDispatcher("webapp/WEB-INF/views/index.jsp").forward(req, resp);
+        System.out.println("ItemsController was called");
+        req.getRequestDispatcher("webapp/WEB-INF/views/items.jsp").forward(req, resp);
     }
-
 }
