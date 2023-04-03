8ed27b414b1f03f38ec15c71b6f0870153e5b432
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-04-03 14:30:41 +0300

    modified web.xml,added annotation

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,34 @@
 package mate.controller;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
+@WebServlet(urlPatterns = "/index")
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String saying = "Hello get";
+        req.setAttribute("message", saying);
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        myResource = MyCoolResource.openResource();
+        myResource.write("it`s init!");
+    }
 }
