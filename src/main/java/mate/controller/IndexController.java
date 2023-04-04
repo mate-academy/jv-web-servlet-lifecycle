8b408fc775635662d89ec6d74145a7b3f4501eca
z1row <pmeysarosh2004@gmail.com>
2023-04-04 21:19:46 +0200

    commit

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,12 +1,10 @@
 package mate.controller;
 
 import java.io.IOException;
-import java.time.ZonedDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
 
