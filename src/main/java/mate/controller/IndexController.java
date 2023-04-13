b77efb9980f73591a57a4173eb9cf3abcd69009f
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-04-13 14:54:09 +0200

    Version 1

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,27 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/webapp/views/index.jsp").forward(req, resp);
+        Date date = new Date();
+        myResource.write(String.valueOf(date));
+        myResource.close();
+    }
 }
