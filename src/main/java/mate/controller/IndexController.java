def131a34c21e3e60e1820f4ee2791e3f9b254ef
Artem Bordichuk <artyombordichuk@gmail.com>
2023-04-15 23:53:42 +0300

    Commit 2: 1. JSP page that display "Information written to our resource" when we write to the resource was implemented. 2. MyCoolResource was initialized trough init() method. 3. The write() method from MyCoolResource  was used in doGet() method. 4. The resource was closed in destroy() method.

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -22,9 +22,9 @@ public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String date = LocalDateTime.now().toString();
+        String localDateTime = LocalDateTime.now().toString();
         myResource.write("date");
-        req.setAttribute("localDateTime", date);
+        req.setAttribute("localDateTime", localDateTime);
         req.getRequestDispatcher("/WEB-INF/views/index.jsp")
                 .forward(req, resp);
     }
