c3fe52824ace70418ce16aaeb6fda6ca9082ecdd
Oleksii Parkhomenko <kcas.pilot@gmail.com>
2023-05-31 21:04:59 +0300

    jv-web-servlet-lifecycle first solution

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,31 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init(ServletConfig config) {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
+        myResource.write("Current date is: " + LocalDateTime.now());
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
