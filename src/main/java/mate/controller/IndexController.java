32f8c1dc42c19e6fe9f59442ddc53a66b3bbea76
Denis Kalyakin <denis.kalyakin999@gmail.com>
2023-06-15 15:58:40 +0300

    task jv-web-servlet-lifecycle-solution is completed1

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,12 +1,12 @@
 package mate.controller;
 
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
