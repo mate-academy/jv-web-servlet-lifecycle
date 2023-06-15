06cff0933a6b2e494cb70f7c50bff78ded531035
Denis Kalyakin <denis.kalyakin999@gmail.com>
2023-06-15 15:56:36 +0300

    task jv-web-servlet-lifecycle-solution is completed

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,34 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
 import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String localDateTime = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("HH:mm:ss.SSSS - dd.MM.yyyy"));
+        myResource.write(localDateTime);
+        req.setAttribute("dateTime", localDateTime);
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
 }
