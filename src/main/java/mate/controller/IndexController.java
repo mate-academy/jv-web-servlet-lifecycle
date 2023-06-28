7b0d38b91d9df2f29281070e7d03baf21abc8e8c
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-06-28 11:40:51 +0300

    create jv-web-servlet-lifecycle

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,15 +1,13 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mate.dao.MyCoolResource;
-
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
