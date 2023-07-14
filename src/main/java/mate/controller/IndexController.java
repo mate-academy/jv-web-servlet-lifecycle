fca9ff920a558e876fc75cdf654c712b2cdc1132
Yura Khilko <yurakhilko@gmail.com>
2023-07-14 16:43:47 +0300

    added fixes3.

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController extends HttpServlet {
     }
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         myResource = MyCoolResource.openResource();
     }
 }
