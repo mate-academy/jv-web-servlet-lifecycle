9df4f7d25bc6d605e2649798330a0d1cec0996ea
Yura Khilko <yurakhilko@gmail.com>
2023-07-14 16:20:15 +0300

    added fixes.

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,6 +1,7 @@
 package mate.controller;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +10,12 @@ import mate.dao.MyCoolResource;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    private int counter = 0;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
-        myResource.write("counter " + ++counter);
+        myResource.write(LocalDateTime.now().toString());
     }
 
     @Override
