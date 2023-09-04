6acc75bbecbc7465a6464c7bf42f89cad3931822
Александр Загородний <git.hub.zagar@gmail.com>
2023-09-04 23:23:37 +0200

    implemented closing initialization methods, and writing to the page in the browserÐÐ

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,35 @@
 package mate.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.dao.MyCoolResource;
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
-    //TODO: implement
+
+    @Override
+    public void init() throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String currentDate = LocalDateTime.now().toString();
+        myResource.write(currentDate);
+
+        req.setAttribute("currentDate", currentDate);
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        if (myResource != null) {
+            myResource.close();
+        }
+    }
 }
