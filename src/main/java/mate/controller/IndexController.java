fe9f45eb199a8d9fb505fafdf1305a8f7647883e
golov <holovkod12@gmail.com>
2023-09-24 17:53:00 +0300

    second attempt

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -2,7 +2,6 @@ package mate.controller;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,11 +11,8 @@ import mate.dao.MyCoolResource;
 public class IndexController extends HttpServlet {
     private MyCoolResource myResource;
 
-    //TODO: implement
-
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+    public void init() throws ServletException {
         myResource = MyCoolResource.openResource();
     }
 
@@ -30,6 +26,5 @@ public class IndexController extends HttpServlet {
     @Override
     public void destroy() {
         myResource.close();
-        super.destroy();
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,11 +1,4 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: golov
-  Date: 9/24/2023
-  Time: 3:13 PM
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <head>
     <title>Title</title>
