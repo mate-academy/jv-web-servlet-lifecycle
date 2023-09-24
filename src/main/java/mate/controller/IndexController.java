c28b980d21167cdc6cfe100ea4d23ba6298f31ea
golov <holovkod12@gmail.com>
2023-09-24 16:59:22 +0300

    all done again

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -15,6 +15,7 @@ public class IndexController extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
+        super.init();
         myResource = MyCoolResource.openResource();
     }
 
@@ -28,5 +29,6 @@ public class IndexController extends HttpServlet {
     @Override
     public void destroy() {
         myResource.close();
+        super.destroy();
     }
 }
